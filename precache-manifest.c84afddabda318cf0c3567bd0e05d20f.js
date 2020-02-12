4b208e9fe22bebece866ffd938986445aad95398
dima-webdev <nguyendmitry@gmail.com>
2020-02-12 21:30:53 +0300

    Updates

---
+++ precache-manifest.c84afddabda318cf0c3567bd0e05d20f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059323ac02286c7c32e959a5ed3891d7",
+    "url": "/simpleCRM/index.html"
+  },
+  {
+    "revision": "d8c943de287d40d19ba2",
+    "url": "/simpleCRM/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9c9ddccf1b8ceea126de",
+    "url": "/simpleCRM/static/css/main.a98b30cb.chunk.css"
+  },
+  {
+    "revision": "d8c943de287d40d19ba2",
+    "url": "/simpleCRM/static/js/2.407f1393.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/simpleCRM/static/js/2.407f1393.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c9ddccf1b8ceea126de",
+    "url": "/simpleCRM/static/js/main.53431cfd.chunk.js"
+  },
+  {
+    "revision": "2ddddaf6e8d7c7da1c47",
+    "url": "/simpleCRM/static/js/runtime-main.db32b380.js"
+  }
+]);
